babe3bdf7ce176204e39169b69b045c339e7a86e
Attila <atti80@gmail.com>
2025-03-19 14:31:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+  var tl = gsap.timeline();
+  //   var tl2 = gsap.timeline();
+  //   var tl3 = gsap.timeline();
+  //   tl.fromTo(
+  //     "#bekesnikolett",
+  //     { opacity: 0 },
+  //     { opacity: 1, delay: 3, duration: 3, yoyo: true, repeat: -1 }
+  //   );
+
+  //   tl.fromTo("body", { opacity: 0 }, { opacity: 1, duration: 1 });
+});
