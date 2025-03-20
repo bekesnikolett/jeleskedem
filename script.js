4110a9872fd86c3f358699ed99ec5a3f7e1b03b5
Attila <atti80@gmail.com>
2025-03-20 19:10:44 +0100

    animations, responsive design

--- script.js
+++ script.js
@@ -2,6 +2,50 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
+  gsap.fromTo(
+    "#altsuli-container",
+    {
+      opacity: 0,
+      y: 100,
+    },
+    {
+      scrollTrigger: "#prices-container",
+      opacity: 1,
+      y: 0,
+      duration: 0.7,
+    }
+  );
+
+  gsap.fromTo(
+    "#kozepsuli-container",
+    {
+      opacity: 0,
+      y: 100,
+    },
+    {
+      scrollTrigger: "#prices-container",
+      opacity: 1,
+      y: 0,
+      duration: 0.7,
+      delay: 0.3,
+    }
+  );
+
+  gsap.fromTo(
+    "#felnott-container",
+    {
+      opacity: 0,
+      y: 100,
+    },
+    {
+      scrollTrigger: "#prices-container",
+      opacity: 1,
+      y: 0,
+      duration: 0.7,
+      delay: 0.6,
+    }
+  );
+
   var btnIntro = document.getElementById("intro-link");
   btnIntro.addEventListener("mouseenter", () => {
     gsap.to("#intro-bg", { top: 0, duration: 0.2 });
